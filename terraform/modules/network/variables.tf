10332a6ece82835a179c5813afb056abae0f91e8
XueFeng <gulyxxf@gmail.com>
2022-08-04 21:33:19 +1000

    update

---
+++ terraform/modules/network/variables.tf
@@ -0,0 +1,10 @@
+variable "cidr_block" {
+  type        = string
+  description = "CIDR block for the VPC and subnet. This value will determine the private IP addresses of the Kubernetes cluster nodes."
+  default     = "172.31.0.0/16"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,21 +1,14 @@
 output "cluster_name" {
-  value       = local.cluster_name
+  value       = module.cluster.cluster_name
   description = "Name of the created cluster."
 }
 
 output "cluster_nodes" {
-  value = [
-    for i in concat([aws_instance.master], aws_instance.workers, ) : {
-      name       = i.tags["node"]
-      subnet_id  = i.subnet_id
-      private_ip = i.private_ip
-      public_ip  = i.tags["node"] == "master" ? aws_eip.master.public_ip : i.public_ip
-    }
-  ]
+  value       = module.cluster.cluster_nodes
   description = "Name, public and private IP address, and subnet ID of all nodes of the created cluster."
 }
 
 output "vpc_id" {
-  value       = aws_security_group.egress.vpc_id
+  value       = module.network.vpc_id
   description = "ID of the VPC in which the cluster has been created."
 }
